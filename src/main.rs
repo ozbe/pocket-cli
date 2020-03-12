2d41ffc1eece133a4f7b91f671f96bb85294dbf6
ozbe <1372945+ozbe@users.noreply.github.com>
2020-03-11 18:41:12 -0700

    refactor get to test success

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate pocket;
 extern crate structopt;
 
-use pocket::Pocket;
+use pocket::{Pocket, PocketGetRequest, PocketResult, PocketItem};
 use std::io;
 use structopt::StructOpt;
 
@@ -52,24 +52,90 @@ fn add(url: &String, opts: &Opts) {
     println!("item: {:?}", item);
 }
 
-fn get(opts: &Opts) {
-    let pocket = Pocket::new(
-        &opts.consumer_key,
-        opts.access_token.as_deref(),
-    );
+fn get(pocket: &impl PocketGet, _opts: &Opts, mut writer: impl std::io::Write) {
     let items = {
         let f = pocket.filter();
         pocket.get(&f)
-    };
-    println!("items: {:?}", items);
+    }.unwrap();
+    writeln!(writer, "items: {:?}", items).unwrap();
 }
 
 fn main() {
     let opts = Opts::from_args();
 
     match opts.command {
-        Commands::Auth(ref sc) => auth(sc,&opts),
+        Commands::Auth(ref sc) => auth(sc, &opts),
         Commands::Add { ref url } => add(&url, &opts),
-        Commands::Get => get(&opts)
+        Commands::Get => {
+            let pocket = Pocket::new(
+                &opts.consumer_key,
+                opts.access_token.as_deref(),
+            );
+            get(&pocket, &opts, &mut std::io::stdout())
+        }
+    }
+}
+
+trait PocketGet {
+    fn filter(&self) -> PocketGetRequest;
+    fn get(&self, request: &PocketGetRequest) -> PocketResult<Vec<PocketItem>>;
+}
+
+impl PocketGet for Pocket {
+    fn filter(&self) -> PocketGetRequest {
+        self.filter()
+    }
+
+    fn get(&self, request: &PocketGetRequest) -> PocketResult<Vec<PocketItem>> {
+        self.get(request)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Commands, PocketGet, get};
+    use pocket::{PocketItem, PocketResult, PocketGetRequest};
+    use crate::Opts;
+
+    struct PocketGetMock<'a, F, G>
+        where
+            F: Fn() -> PocketGetRequest<'a>,
+            G: Fn(&PocketGetRequest) -> PocketResult<Vec<PocketItem>>,
+    {
+        filter_mock: F,
+        get_mock: G
+    }
+
+    impl<'a, F, G> PocketGet for PocketGetMock<'a, F, G>
+        where
+        F: Fn() -> PocketGetRequest<'a>,
+        G: Fn(&PocketGetRequest) -> PocketResult<Vec<PocketItem>>,
+    {
+        fn filter(&self) -> PocketGetRequest {
+            (self.filter_mock)()
+        }
+
+        fn get(&self, request: &PocketGetRequest) -> PocketResult<Vec<PocketItem>> {
+            (self.get_mock)(request)
+        }
+    }
+
+    #[test]
+    fn get_success() {
+        let items: Vec<PocketItem> = vec![];
+        let pocket = PocketGetMock {
+            filter_mock: || PocketGetRequest::new(),
+            get_mock: |_| Ok(vec![]),
+        };
+        let opts = Opts {
+            consumer_key: "".to_string(),
+            access_token: None,
+            command: Commands::Get
+        };
+        let mut result = Vec::new();
+
+        get(&pocket, &opts, &mut result);
+
+        assert_eq!(format!("items: {:?}\n", &items).into_bytes(), result);
     }
 }
